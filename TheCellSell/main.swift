4ff062c435d961b0775065905cbe4bf8c9a90b68
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2020-10-26 17:28:28 -0400

    Completed Cell Sell

--- TheCellSell/main.swift
+++ TheCellSell/main.swift
@@ -28,27 +28,35 @@ let day = Int(readLine()!)!
 print("Number of evening minutes?")
 let evening = Int(readLine()!)!
 
+// Get weekend minutes
+print("Number of weekend minutes?")
+let weekend = Int(readLine()!)!
+
 // PROCESS
 
 // Calculate cost for plan A
 var a = 0
 
 // Add daytime cost
-a += (day - 100) * 25
+if day > 100 {
+    a += (day - 100) * 25
+}
 
 // Add evening cost
 a += evening * 15
 
+a += weekend * 20
 // Calculate cost for plan B
 var b = 0
 
 // Add daytime cost
-b += (day - 250) * 45
-
+if day > 250 {
+    b += (day - 250) * 45
+}
 // Add evening cost
 b += evening * 35
 
-
+b += weekend * 25
 // OUTPUT
 print("Plan A costs \(a)")
 print("Plan B costs \(b)")
@@ -56,5 +64,9 @@ print("Plan B costs \(b)")
 if a > b {
     print("Plan B is cheapest.")
 } else {
-    print("Plan A is cheapest.")
+    if a < b {
+        print("Plan A is cheapest.")
+    } else {
+        print("Plan A and Plan B are the same price.")
+    }
 }
