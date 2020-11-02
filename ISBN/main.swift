0c7a60d3af853d8ebbad4fc4b63dd54415c4dcac
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2020-11-02 16:38:29 -0500

    Solved ISBN

--- ISBN/main.swift
+++ ISBN/main.swift
@@ -20,18 +20,25 @@ let first10Digits = "9780921418"
 
 // Set the sum of just the first 10 digits, which are always 9780921418
 // TODO: Use puzzle description to determine how much to subtract.
-let first10DigitsSum = 120 - 0
+var Sum = 91
 
 // Ask for the 11th digit
 print("Digit 11? ", terminator: "")
 let digit11 = Int(readLine()!)!
-
-let digit12 = 7
-
-let digit13 = 9
+print("Digit 12? ", terminator: "")
+let digit12 = Int(readLine()!)!
+print("Digit 13? ", terminator: "")
+let digit13 = Int(readLine()!)!
 
 // PROCESS & OUTPUT
-
+Sum += digit11 + digit12 * 3 + digit13
+print("The 1-3-sum is \(Sum)")
+if Sum % 10 == 0 {
+    print("The ISBN number \(first10Digits)\(digit11)\(digit12)\(digit13) is a valid ISBN number.")
+}
+else {
+    print("The ISBN number 9780921418052 is NOT a valid ISBN number.")
+}
 // Finish calculating the sum including the 11th, 12th, and 13th digits of the ISBN number
 
 // Print the 1-3 sum for the entire 13-digit ISBN number
