05d0e917fe466af99a1c6561dfef7fa79001fbd1
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2020-11-02 16:25:39 -0500

    Solved SpeedingIsNotFine

--- SpeedingIsNotFine/main.swift
+++ SpeedingIsNotFine/main.swift
@@ -18,14 +18,23 @@ print("====================")
 // Ask for the speed limit
 print("What is the speed limit, in km/h? ", terminator: "")
 let limit = Int(readLine()!)!
-
-let speed = 45
-
+print("What is the recorded speed, in km/h? ", terminator: "")
+let speed = Int(readLine()!)!
 // PROCESS AND OUTPUT
 
 // See whether the driver is within the speed limit, and if they are not, see how much they are above the limit.
 if speed < limit {
     print("Congratulations, you are within the speed limit!")
 } else {
-    print("You are speeding.")
+    if speed - 21 < limit {
+        print("You are speeding and your fine is $100.")
+    }
+    else {
+        if speed - 31 < limit {
+            print("You are speeding and your fine is $270.")
+        }
+        else {
+            print("You are speeding and your fine is $500.")
+        }
+    }
 }
