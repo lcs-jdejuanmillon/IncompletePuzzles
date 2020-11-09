d55e989ed7fb5444d152e49ec0fcee0979f35a90
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2020-11-09 16:28:56 -0500

    .

--- ChipsFastFood/main.swift
+++ ChipsFastFood/main.swift
@@ -32,13 +32,16 @@ print("Welcome to Chip's Fast Food Emporium")
 //       Then drag and drop Extensions.swift to the Compile Sources subsection.
 //
 let burger = Int.collectInput(withPrompt: "Please enter a burger choice:", minimum: 1, maximum: 4)
-let drink = 3
-let side = 4
-let dessert = 1
-
+let drink = Int.collectInput(withPrompt: "Please enter a drink choice:", minimum: 1, maximum: 4)
+let side = Int.collectInput(withPrompt: "Please enter a side choice:", minimum: 1, maximum: 4)
+let dessert = Int.collectInput(withPrompt: "Please enter a dessert choice:", minimum: 1, maximum: 4)
 // PROCESS
 // NOTE: Give some thought to what kind of data structure would be helpful for this program.
 //       Perhaps a collection type of some sort?
-
+let bur = [461, 431, 420, 0]
+let dri = [130, 160, 118, 0]
+let sid = [100, 57, 70, 0]
+let des = [167, 266, 75, 0]
 // OUTPUT
 // Show the calories total for the choices made by the user.
+print("Your total Calorie count is \(bur[burger] + dri[drink] + sid[side] + des[dessert]).")
